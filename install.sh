7a2799c6e6b3d2ede7a4505382f9b91ecaffd2ed
Eunchurn Park <eunchurn@onycom.com>
2024-06-10 16:09:31 +0900

    fix(nvm): installer

--- install.sh
+++ install.sh
@@ -107,36 +107,36 @@ if [ $(confirm "Do you want to install mysql-5.7.44?") -eq "1" ]; then
   # systemctl start mysqld
 fi
 
-echo "[IMQA] Installing NVM with Node12 & Node18"
-if [ $(confirm "Do you want to install NVM with Node12 & Node18?") -eq "1" ]; then
-  NVM_PACKAGE_FILE=nvm-packed-12-18.tar.xz
-  BASHRC=$HOME/.bashrc
-  NVM_EXPORT='export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
-  if [ ! -d "$HOME/.nvm" ]; then
-    if [ -f "$NVM_PACKAGE_FILE" ]; then
-      echo "Installing nvm from $NVM_PACKAGE_FILE"
-      tar -xJvf "$NVM_PACKAGE_FILE" -C $HOME
-      sh $HOME/.nvm/install.sh
-    else
-      echo "$NVM_PACKAGE_FILE is not exist in $HOME"
-      exit 1
-    fi
-  else
-    echo "[IMQA] NVM already installed"
-  fi
-  TEMP_FILE=$(mktemp)
-  echo "$NVM_EXPORT" > "$TEMP_FILE"
+# echo "[IMQA] Installing NVM with Node12 & Node18"
+# if [ $(confirm "Do you want to install NVM with Node12 & Node18?") -eq "1" ]; then
+#   NVM_PACKAGE_FILE=nvm-packed-12-18.tar.xz
+#   BASHRC=$HOME/.bashrc
+#   NVM_EXPORT='export NVM_DIR="$HOME/.nvm"
+#   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
+#   if [ ! -d "$HOME/.nvm" ]; then
+#     if [ -f "$NVM_PACKAGE_FILE" ]; then
+#       echo "Installing nvm from $NVM_PACKAGE_FILE"
+#       tar -xJvf "$NVM_PACKAGE_FILE" -C $HOME
+#       sh $HOME/.nvm/install.sh
+#     else
+#       echo "$NVM_PACKAGE_FILE is not exist in $HOME"
+#       exit 1
+#     fi
+#   else
+#     echo "[IMQA] NVM already installed"
+#   fi
+#   TEMP_FILE=$(mktemp)
+#   echo "$NVM_EXPORT" > "$TEMP_FILE"
   
-  if ! grep -F "$(cat "$TEMP_FILE")" "$BASHRC" > /dev/null; then
-    cat "$TEMP_FILE" >> "$BASHRC"
-  fi
+#   if ! grep -F "$(cat "$TEMP_FILE")" "$BASHRC" > /dev/null; then
+#     cat "$TEMP_FILE" >> "$BASHRC"
+#   fi
 
-  rm -f "$TEMP_FILE"
-  echo "All setup"
-  echo "Reload bash profile 'source ~/.bashrc'"
-fi
+#   rm -f "$TEMP_FILE"
+#   echo "All setup"
+#   echo "Reload bash profile 'source ~/.bashrc'"
+# fi
 
 echo "[IMQA] Installation Done"
 echo "Please restart your terminal to apply changes"
