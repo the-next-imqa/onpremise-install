bac34395a1d6135fc01e6ddcea062d2c50971f7a
Eunchurn Park <eunchurn@onycom.com>
2024-06-10 10:26:24 +0900

    chore(nvm): check

--- install.sh
+++ install.sh
@@ -113,18 +113,21 @@ if [ $(confirm "Do you want to install NVM with Node12 & Node18?") -eq "1" ]; th
   NVM_EXPORT='export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
-
-  if [ -f "$NVM_PACKAGE_FILE" ]; then
-    echo "Installing nvm from $NVM_PACKAGE_FILE"
-    tar -xJvf "$NVM_PACKAGE_FILE" -C $HOME
-    sh $HOME/.nvm/install.sh
-    sed -i "/$NVM_EXPORT/d" $BASHRC
-    echo $NVM_EXPORT >> $BASHRC
-    echo "All setup"
-    echo "Reload bash profile `source ~/.bashrc`"
+  if [ ! -d "$PWD/.nvm" ]; then
+    if [ -f "$NVM_PACKAGE_FILE" ]; then
+      echo "Installing nvm from $NVM_PACKAGE_FILE"
+      tar -xJvf "$NVM_PACKAGE_FILE" -C $HOME
+      sh $HOME/.nvm/install.sh
+      sed -i "/$NVM_EXPORT/d" $BASHRC
+      echo $NVM_EXPORT >> $BASHRC
+      echo "All setup"
+      echo "Reload bash profile `source ~/.bashrc`"
+    else
+      echo "$NVM_PACKAGE_FILE is not exist in $HOME"
+      exit 1
+    fi
   else
-    echo "$NVM_PACKAGE_FILE is not exist in $HOME"
-    exit 1
+    echo "[IMQA] NVM already installed"
   fi
 fi
 
