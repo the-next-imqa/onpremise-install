1457a77bee1a92f03353aad4d2446228ec308048
Eunchurn Park <eunchurn@onycom.com>
2024-06-10 12:34:34 +0900

    refactor(prepare.sh): gpt

--- prepare.sh
+++ prepare.sh
@@ -7,25 +7,26 @@ download_if_not_exists() {
 
   if [ ! -e "$target" ]; then
     wget "$url" -P "$(dirname "$target")"
+    echo "[IMQA] Downloaded $description"
   else
-    echo "[IMQA] $description exist. Skipping..."
+    echo "[IMQA] $description already exists. Skipping..."
   fi
 }
 
-# download IMQA packages
+# Download IMQA packages
 IMQA_PACKAGE_FILE="rhel8.x-imqa-packages.tar.xz"
 download_if_not_exists "https://cdn.oh.camp/install/$IMQA_PACKAGE_FILE" "$IMQA_PACKAGE_FILE" "IMQA package file"
 
-# download PM2
+# Download PM2
 PM2_LOCATION="pm2"
 if [ ! -d "$PM2_LOCATION" ]; then
   echo "[IMQA] Creating directory: $PWD/$PM2_LOCATION"
   mkdir -p "$PM2_LOCATION"
 fi
 
-download_if_not_exists "https://cdn.oh.camp/install/pm2-installer-node-v12.22.12.tar.gz" "$PM2_LOCATION/pm2-installer-node-v12.22.12.tar.gz" "PM2 installer Node12"
-download_if_not_exists "https://cdn.oh.camp/install/pm2-installer-node-v18.20.3.tar.gz" "$PM2_LOCATION/pm2-installer-node-v18.20.3.tar.gz" "PM2 installer Node18"
-download_if_not_exists "https://cdn.oh.camp/install/pm2-logrotate-2.7.0-node-v12.22.12.tar.gz" "$PM2_LOCATION/pm2-logrotate-2.7.0-node-v12.22.12.tar.gz" "PM2 logrotate 2.7.0 Node12"
-download_if_not_exists "https://cdn.oh.camp/install/pm2-logrotate-2.7.0-node-v18.20.3.tar.gz" "$PM2_LOCATION/pm2-logrotate-2.7.0-node-v18.20.3.tar.gz" "PM2 logrotate 2.7.0 Node18"
+download_if_not_exists "https://cdn.oh.camp/install/pm2-installer-node-v12.22.12.tar.gz" "$PM2_LOCATION/pm2-installer-node-v12.22.12.tar.gz" "PM2 installer for Node.js v12.22.12"
+download_if_not_exists "https://cdn.oh.camp/install/pm2-installer-node-v18.20.3.tar.gz" "$PM2_LOCATION/pm2-installer-node-v18.20.3.tar.gz" "PM2 installer for Node.js v18.20.3"
+download_if_not_exists "https://cdn.oh.camp/install/pm2-logrotate-2.7.0-node-v12.22.12.tar.gz" "$PM2_LOCATION/pm2-logrotate-2.7.0-node-v12.22.12.tar.gz" "PM2 logrotate v2.7.0 for Node.js v12.22.12"
+download_if_not_exists "https://cdn.oh.camp/install/pm2-logrotate-2.7.0-node-v18.20.3.tar.gz" "$PM2_LOCATION/pm2-logrotate-2.7.0-node-v18.20.3.tar.gz" "PM2 logrotate v2.7.0 for Node.js v18.20.3"
 
-echo "[IMQA] Downloads completed"
+echo "[IMQA] All downloads completed"
