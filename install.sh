2f8191e650a145d81497eb863d56f80bdbcb5716
Eunchurn Park <eunchurn@onycom.com>
2024-06-10 10:30:46 +0900

    fix(nvm): bashrc

--- install.sh
+++ install.sh
@@ -110,8 +110,8 @@ echo "[IMQA] Installing NVM with Node12 & Node18"
 if [ $(confirm "Do you want to install NVM with Node12 & Node18?") -eq "1" ]; then
   NVM_PACKAGE_FILE=nvm-packed-12-18.tar.xz
   BASHRC=$HOME/.bashrc
-  NVM_EXPORT='export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  NVM_EXPORT='export NVM_DIR="$HOME/.nvm"\n
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm\n
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
   if [ ! -d "$HOME/.nvm" ]; then
     if [ -f "$NVM_PACKAGE_FILE" ]; then
@@ -119,7 +119,7 @@ if [ $(confirm "Do you want to install NVM with Node12 & Node18?") -eq "1" ]; th
       tar -xJvf "$NVM_PACKAGE_FILE" -C $HOME
       sh $HOME/.nvm/install.sh
       sed -i "/$NVM_EXPORT/d" $BASHRC
-      echo $NVM_EXPORT >> $BASHRC
+      echo -e $NVM_EXPORT >> $BASHRC
       echo "All setup"
       echo "Reload bash profile `source ~/.bashrc`"
     else
