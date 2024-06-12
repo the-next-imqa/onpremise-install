c1de307ec1e92a8ad47c8be83e17e759a19b8b59
Eunchurn Park <eunchurn@onycom.com>
2024-06-12 18:02:55 +0900

    wip(rabbitmq): wip

--- configure-non-sudo.sh
+++ configure-non-sudo.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
+# Directories
 export CONFIG_DIR="$HOME/.config"
 export SYSTEMD_DIR="$CONFIG_DIR/systemd"
 export USER_DIR="$SYSTEMD_DIR/user"
 export SBIN_DIR="$HOME/.sbin"
 export SCRIPT_PATH="$HOME/.scripts"
 
+# Script paths
 CONFIGURE_MYSQL_SCRIPT="$PWD/scriptV2/config_mysql.sh"
 CONFIGURE_REDIS_SCRIPT="$PWD/scriptV2/config_redis.sh"
 
+# Function to confirm user input
 confirm() {
   local yn
   while true; do
     read -p "$1 [y/N] : " yn
     yn=${yn:-n}
     case "$yn" in
-    [Yy]*) 
-      return 0
-      ;;
-    [Nn]*)
-      return 1
-      ;;
+      [Yy]*) return 0 ;;
+      [Nn]*) return 1 ;;
     esac
   done
 }
 
 export -f confirm
 
+# Function to create directory if it does not exist
 create_dir() {
   if [ ! -d "$1" ]; then
     echo "Creating $1 directory..."
@@ -38,6 +38,7 @@ create_dir() {
 
 export -f create_dir
 
+# Function to read user input with a default value
 read_input() {
   local prompt=$1
   local default=$2
@@ -48,6 +49,36 @@ read_input() {
 
 export -f read_input
 
+# Function to handle service registration
+handle_service() {
+  local service_name=$1
+  local config_script=$2
+  local package_name=$3
+
+  if confirm "Do you want to register $service_name service as non-sudo user? ($(whoami))"; then
+    if [ $(rpm -qa | grep -c $package_name) -eq 0 ]; then
+      echo "[IMQA] $service_name is not installed"
+      echo "[IMQA] Please install $service_name first"
+      echo "[IMQA] Aborting $service_name configuration"
+    else
+      echo "[IMQA] $service_name is installed"
+      echo "[IMQA] Checking $service_name status"
+      if [ $(systemctl --user is-active ${package_name}@imqa) == "active" ]; then
+        echo "[IMQA] $service_name is running"
+        echo "[IMQA] Stopping $service_name"
+        systemctl --user stop ${package_name}@imqa
+      else
+        echo "[IMQA] $service_name is not running"
+      fi
+      sh $config_script
+      echo "[IMQA] $service_name configuration done"
+    fi
+  else
+    echo "[IMQA] Registering $service_name server daemon skipped..."
+  fi
+}
+
+# Main script execution
 if [ "$EUID" == 0 ]; then
   echo "Do not run as sudoer"
   exit
@@ -59,54 +90,8 @@ create_dir "$SYSTEMD_DIR"
 create_dir "$USER_DIR"
 create_dir "$SCRIPT_PATH"
 
-# MySQL
-if confirm "Do you want to register MySQL service as non-sudo user? ($(whoami))"; then
-  # Check if mysql is installed
-  if [ $(rpm -qa | grep -c mysql) -eq 0 ]; then
-    echo "[IMQA] MySQL is not installed"
-    echo "[IMQA] Please install MySQL first"
-    echo "[IMQA] Aborting MySQL configuration"
-  else
-    echo "[IMQA] MySQL is installed"
-    echo "[IMQA] Checking MySQL status"
-    # Check if mysql is running
-    if [ $(systemctl --user is-active mysqld@imqa) == "active" ]; then
-      echo "[IMQA] MySQL is running"
-      echo "[IMQA] Stopping MySQL"
-      systemctl --user stop mysqld@imqa
-    else
-      echo "[IMQA] MySQL is not running"
-    fi
-    sh $CONFIGURE_MYSQL_SCRIPT
-    echo "[IMQA] MySQL configuration done"
-  fi
-else
-  echo "[IMQA] Registering MySQL server daemon skipped..."
-fi
-
-# Redis
-if confirm "Do you want to register Redis service as non-root user? ($(whoami))"; then
-  # Check if redis is installed
-  if [ $(rpm -qa | grep -c redis) -eq 0 ]; then
-    echo "[IMQA] Redis is not installed"
-    echo "[IMQA] Please install Redis first"
-    echo "[IMQA] Aborting Redis configuration"
-  else
-    echo "[IMQA] Redis is installed"
-    echo "[IMQA] Checking Redis status"
-    # Check if redis is running
-    if [ $(systemctl --user is-active redis@imqa) == "active" ]; then
-      echo "[IMQA] Redis is running"
-      echo "[IMQA] Stopping Redis service"
-      systemctl --user stop redis@imqa
-    else
-      echo "[IMQA] Redis is not running"
-    fi
-    sh $CONFIGURE_REDIS_SCRIPT
-    echo "[IMQA] Redis configuration done"
-  fi
-else
-  echo "[IMQA] Registering Redis server daemon skipped..."
-fi
+# Register services
+handle_service "MySQL" "$CONFIGURE_MYSQL_SCRIPT" "mysql"
+handle_service "Redis" "$CONFIGURE_REDIS_SCRIPT" "redis"
 
 echo "Script execution completed."
