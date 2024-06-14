cb6f29a78c494e7b66b901246692276fdeccf3bc
Eunchurn Park <eunchurn@onycom.com>
2024-06-14 11:04:38 +0900

    wip(rabbitmq): script

--- scriptV2/config_rabbitmq.sh
+++ scriptV2/config_rabbitmq.sh
@@ -3,6 +3,8 @@
 DEFAULT_RABBITMQ_BASE_PATH="$HOME/rabbitmq"
 RABBITMQ_SERVICE_FILE="rabbitmq-server@imqa.service"
 RABBITMQ_SERVICE_FILE_PATH="$PWD/template/rabbitmq/$RABBITMQ_SERVICE_FILE"
+RABBITMQ_SERVICE_ENV_FILE="rabbitmq-server@imqa"
+RABBITMQ_SERVICE_ENV_FILE_PATH="$PWD/template/rabbitmq/$RABBITMQ_SERVICE_ENV_FILE"
 RABBITMQ_CONFIG="rabbitmq.conf"
 RABBITMQ_CONFIG_PATH="$PWD/template/rabbitmq/$RABBITMQ_CONFIG"
 RABBITMQ_CONFIG_DEFINITION_FILE=definition.json
@@ -23,12 +25,16 @@ export RABBITMQ_USERNAME=$(read_input "Enter the default username for RabbitMQ (
 export RABBITMQ_PASSWORD=$(read_input "Enter the default password for RabbitMQ (default 1234): ", "1234")
 export RABBITMQ_PASSWORD_HASH=$(encode_password $RABBITMQ_PASSWORD)
 
+export RABBITMQ_LOG_PATH=$RABBITMQ_BASE_PATH
+
 create_dir "$RABBITMQ_BASE_PATH"
 create_dir "$RABBITMQ_CONFIG_FILES"
 
 echo "Registering MySQL service system daemon of non-sudo user"
 
 if [ -f "$RABBITMQ_SERVICE_FILE_PATH" ]; then
+  echo "Temaplating $RABBITMQ_SERVICE_ENV_FILE_PATH"
+  envsubst <"$RABBITMQ_SERVICE_ENV_FILE_PATH" >"$USER_DIR/$RABBITMQ_SERVICE_ENV_FILE"
   echo "Templating $RABBITMQ_SERVICE_FILE_PATH"
   envsubst <"$RABBITMQ_SERVICE_FILE_PATH" >"$USER_DIR/$RABBITMQ_SERVICE_FILE"
   echo "Templating $RABBITMQ_CONFIG"
